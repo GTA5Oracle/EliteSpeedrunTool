e8ecc728f50058566dcd56e891c6d54cf058b2dc
SkyD666 <akarinnn666@gmail.com>
2025-02-13 21:28:12 +0800

    [optimize] Three methods for terminating game process in parallel

--- EliteSpeedrunTool/MainFeatures.cpp
+++ EliteSpeedrunTool/MainFeatures.cpp
@@ -3,13 +3,27 @@
 #include "MemoryUtil.h"
 #include "event/Event.h"
 #include <QWidget>
+#include <QtConcurrent>
 #include <ntdef.h>
+#include <windows.h>
 
 Q_GLOBAL_STATIC(MainFeatures, mainFeaturesInstance)
 
 MainFeatures::MainFeatures(QObject* parent)
     : QObject { parent }
 {
+    auto ntdllHandle = GetModuleHandleW(L"ntdll.dll");
+    auto user32Handle = GetModuleHandleW(L"User32.dll");
+    if (ntdllHandle) {
+        NtTerminateProcess = (PNtTerminateProcess)GetProcAddress(ntdllHandle, "NtTerminateProcess");
+    } else {
+        qCritical() << "MainFeatures: Cannot get ntdll.dll's module handle";
+    }
+    if (user32Handle) {
+        EndTask = (PEndTask)GetProcAddress(user32Handle, "EndTask");
+    } else {
+        qCritical() << "MainFeatures: Cannot get User32.dll's module handle";
+    }
 }
 
 MainFeatures* MainFeatures::instance()
@@ -17,12 +31,26 @@ MainFeatures* MainFeatures::instance()
     return mainFeaturesInstance;
 }
 
-typedef NTSTATUS(NTAPI* PNtTerminateProcess)(HANDLE, NTSTATUS);
-
 void MainFeatures::terminateGta()
 {
-    const auto gamePid = memoryUtil->getGtaPid();
-    if (!terminateProcess(gamePid) && !ntTerminateProcess(gamePid)) {
+    const HWND hwnd = memoryUtil->getGtaWindowHwnd();
+    const DWORD gamePid = memoryUtil->getPid(hwnd);
+
+    QFuture<bool> futures[] = {
+        QtConcurrent::run(&MainFeatures::ntTerminateProcess, this, gamePid),
+        QtConcurrent::run(&MainFeatures::terminateProcess, this, gamePid),
+        QtConcurrent::run(&MainFeatures::endTask, this, hwnd),
+    };
+    auto result = false;
+    for (int i = 0; i < sizeof(futures) / sizeof(futures[0]); i++) {
+        if (futures[i].result()) {
+            result = true;
+            qInfo() << "terminateGta future:" << i << "successful";
+            break;
+        }
+    }
+
+    if (!result) {
         WinExec("taskkill /f /t /im GTA5.exe", SW_HIDE);
         qWarning("Using taskkill command to terminate GTA5.exe");
     }
@@ -47,21 +75,22 @@ void MainFeatures::applyWindowDisplayAffinity(QWidget* widget)
 
 bool MainFeatures::ntTerminateProcess(DWORD pid)
 {
-    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
     bool res = false;
-    if (hProcess) {
-        PNtTerminateProcess ntTerminateProcess = (PNtTerminateProcess)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtTerminateProcess");
-        if (ntTerminateProcess) {
-            if (NT_SUCCESS(ntTerminateProcess(hProcess, 0))) {
-                qInfo("Execute NtTerminateProcess API succeeds");
+    if (NtTerminateProcess) {
+        HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+        if (hProcess) {
+            if (NT_SUCCESS(NtTerminateProcess(hProcess, 0))) {
+                qInfo("Execute NtTerminateProcess succeeds");
                 res = true;
             } else {
                 qWarning("Execute NtTerminateProcess fails");
             }
+            CloseHandle(hProcess);
         } else {
-            qWarning("PNtTerminateProcess is null");
+            qWarning("ntTerminateProcess: OpenProcess fails");
         }
-        CloseHandle(hProcess);
+    } else {
+        qWarning("NtTerminateProcess is null");
     }
     return res;
 }
@@ -71,11 +100,26 @@ bool MainFeatures::terminateProcess(DWORD pid)
     HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
     bool res = false;
     if (TerminateProcess(hProcess, 1)) {
-        qInfo("Execute TerminateProcess API succeeds");
+        qInfo("Execute TerminateProcess succeeds");
         res = true;
     } else {
         qWarning("Execute TerminateProcess fails");
     }
     CloseHandle(hProcess);
+    return res;
+}
+
+bool MainFeatures::endTask(HWND hwnd)
+{
+    if (!EndTask) {
+        return false;
+    }
+    bool res = false;
+    if (EndTask(hwnd, FALSE, TRUE)) {
+        qInfo("Execute EndTask succeeds");
+        res = true;
+    } else {
+        qWarning("Execute EndTask fails");
+    }
     return res;
 }
--- EliteSpeedrunTool/MainFeatures.h
+++ EliteSpeedrunTool/MainFeatures.h
@@ -5,6 +5,12 @@
 
 #define mainFeatures (MainFeatures::instance())
 
+typedef NTSTATUS(NTAPI* PNtTerminateProcess)(HANDLE, NTSTATUS);
+typedef BOOL(WINAPI* PEndTask)(
+    HWND hWnd,
+    BOOL fShutDown,
+    BOOL fForce);
+
 class MainFeatures : public QObject {
     Q_OBJECT
 public:
@@ -17,7 +23,10 @@ public:
     void applyWindowDisplayAffinity(QWidget* widget);
 
 private:
+    PNtTerminateProcess NtTerminateProcess;
+    PEndTask EndTask;
     bool ntTerminateProcess(DWORD pid);
     bool terminateProcess(DWORD pid);
+    bool endTask(HWND hwnd);
 signals:
 };
